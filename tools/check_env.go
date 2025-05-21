41e639851dd1bdc8a52f22ce3e060553636a0967
NullMe <192708205+NullMeDev@users.noreply.github.com>
2025-05-21 05:32:59 -0400

    Update check_env.go

--- tools/check_env.go
+++ tools/check_env.go
@@ -1,1 +1,31 @@
+package main
 
+import (
+    "fmt"
+    "os"
+)
+
+func main() {
+    required := []string{
+        "DISCORD_BOT_TOKEN",
+        "DISCORD_APPLICATION_ID",
+        "DISCORD_GUILD_ID",
+        "DISCORD_CHANNEL_ID",
+    }
+    missing := []string{}
+
+    for _, key := range required {
+        val := os.Getenv(key)
+        if val == "" {
+            missing = append(missing, key)
+        } else {
+            fmt.Printf("%s is set\n", key)
+        }
+    }
+    if len(missing) > 0 {
+        fmt.Printf("Missing environment variables: %v\n", missing)
+        os.Exit(1)
+    } else {
+        fmt.Println("All required environment variables are set.")
+    }
+}
