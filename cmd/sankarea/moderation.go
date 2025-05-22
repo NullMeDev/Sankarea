3a1e72946a0972d1f00333d73ab1967c16e7247d
NullMe <192708205+NullMeDev@users.noreply.github.com>
2025-05-21 21:20:06 -0400

    Update moderation.go

--- cmd/sankarea/moderation.go
+++ cmd/sankarea/moderation.go
@@ -1,143 +1,51 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 
-    "github.com/bwmarrin/discordgo"
+	"github.com/bwmarrin/discordgo"
 )
 
-// Silence a user for given minutes
-func SilenceUser(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    if !isAdminOrOwner(i) {
-        respondNoPrivilege(s, i)
-        return
-    }
-    targetUser := i.ApplicationCommandData().Options[0].UserValue(s)
-    mins := int(i.ApplicationCommandData().Options[1].IntValue())
+// canTarget checks if the command issuer can moderate the target user (no equal or higher roles)
+func canTarget(i *discordgo.InteractionCreate, targetID string) bool {
+	if targetID == discordOwnerID {
+		return false
+	}
 
-    if !canTarget(i, targetUser.ID) {
-        respondCantTarget(s, i)
-        return
-    }
+	userRoles := i.Member.Roles
+	member, err := dg.GuildMember(i.GuildID, targetID)
+	if err != nil {
+		return false
+	}
 
-    until := time.Now().Add(time.Duration(mins) * time.Minute)
-    err := s.GuildMemberTimeout(i.GuildID, targetUser.ID, &until)
-    if err != nil {
-        respondFailure(s, i, "silence user", err)
-        logAudit("SilenceFail", fmt.Sprintf("Attempt on <@%s> by <@%s>: %v", targetUser.ID, i.Member.User.ID, err), 0xff0000)
-        return
-    }
-    logAudit("Silenced", fmt.Sprintf("<@%s> silenced for %d min by <@%s>", targetUser.ID, mins, i.Member.User.ID), 0xffcc00)
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: fmt.Sprintf("User <@%s> silenced for %d minutes.", targetUser.ID, mins),
-        },
-    })
+	for _, rid := range member.Roles {
+		for _, myrid := range userRoles {
+			if rid == myrid {
+				return false
+			}
+		}
+	}
+	return true
 }
 
-// Unsilence user (remove timeout)
-func UnsilenceUser(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    if !isAdminOrOwner(i) {
-        respondNoPrivilege(s, i)
-        return
-    }
-    targetUser := i.ApplicationCommandData().Options[0].UserValue(s)
-    err := s.GuildMemberTimeout(i.GuildID, targetUser.ID, nil)
-    if err != nil {
-        respondFailure(s, i, "unsilence user", err)
-        logAudit("UnsilenceFail", fmt.Sprintf("Attempt on <@%s> by <@%s>: %v", targetUser.ID, i.Member.User.ID, err), 0xff0000)
-        return
-    }
-    logAudit("Unsilenced", fmt.Sprintf("<@%s> unsilenced by <@%s>", targetUser.ID, i.Member.User.ID), 0x00ff00)
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: fmt.Sprintf("User <@%s> unsilenced.", targetUser.ID),
-        },
-    })
+// silenceUser applies timeout (mute) to a user for specified minutes
+func silenceUser(s *discordgo.Session, i *discordgo.InteractionCreate, targetUserID string, minutes int) error {
+	until := time.Now().Add(time.Duration(minutes) * time.Minute)
+	return s.GuildMemberTimeout(i.GuildID, targetUserID, &until)
 }
 
-// Kick a user
-func KickUser(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    if !isAdminOrOwner(i) {
-        respondNoPrivilege(s, i)
-        return
-    }
-    targetUser := i.ApplicationCommandData().Options[0].UserValue(s)
-    if !canTarget(i, targetUser.ID) {
-        respondCantTarget(s, i)
-        return
-    }
-    err := s.GuildMemberDeleteWithReason(i.GuildID, targetUser.ID, "Kicked by Sankarea bot")
-    if err != nil {
-        respondFailure(s, i, "kick user", err)
-        logAudit("KickFail", fmt.Sprintf("Attempt on <@%s> by <@%s>: %v", targetUser.ID, i.Member.User.ID, err), 0xff0000)
-        return
-    }
-    logAudit("Kicked", fmt.Sprintf("<@%s> kicked by <@%s>", targetUser.ID, i.Member.User.ID), 0xff6600)
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: fmt.Sprintf("User <@%s> kicked.", targetUser.ID),
-        },
-    })
+// unsilenceUser removes timeout from a user
+func unsilenceUser(s *discordgo.Session, i *discordgo.InteractionCreate, targetUserID string) error {
+	return s.GuildMemberTimeout(i.GuildID, targetUserID, nil)
 }
 
-// Ban a user
-func BanUser(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    if !isAdminOrOwner(i) {
-        respondNoPrivilege(s, i)
-        return
-    }
-    targetUser := i.ApplicationCommandData().Options[0].UserValue(s)
-    if !canTarget(i, targetUser.ID) {
-        respondCantTarget(s, i)
-        return
-    }
-    err := s.GuildBanCreateWithReason(i.GuildID, targetUser.ID, "Banned by Sankarea bot", 0)
-    if err != nil {
-        respondFailure(s, i, "ban user", err)
-        logAudit("BanFail", fmt.Sprintf("Attempt on <@%s> by <@%s>: %v", targetUser.ID, i.Member.User.ID, err), 0xff0000)
-        return
-    }
-    logAudit("Banned", fmt.Sprintf("<@%s> banned by <@%s>", targetUser.ID, i.Member.User.ID), 0xff0000)
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: fmt.Sprintf("User <@%s> banned.", targetUser.ID),
-        },
-    })
+// kickUser kicks a user from the guild
+func kickUser(s *discordgo.Session, i *discordgo.InteractionCreate, targetUserID string) error {
+	return s.GuildMemberDeleteWithReason(i.GuildID, targetUserID, "Kicked by admin/owner via Sankarea bot")
 }
 
-// Helper responses
-func respondNoPrivilege(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: "Weeb, You Do Not Have The Right Privileges.",
-            Flags:   1 << 6,
-        },
-    })
-}
-
-func respondCantTarget(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: "Cannot perform action on a user with equal or higher permissions.",
-            Flags:   1 << 6,
-        },
-    })
-}
-
-func respondFailure(s *discordgo.Session, i *discordgo.InteractionCreate, action string, err error) {
-    s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
-        Data: &discordgo.InteractionResponseData{
-            Content: fmt.Sprintf("Failed to %s: %v", action, err),
-            Flags:   1 << 6,
-        },
-    })
+// banUser bans a user from the guild
+func banUser(s *discordgo.Session, i *discordgo.InteractionCreate, targetUserID string) error {
+	return s.GuildBanCreateWithReason(i.GuildID, targetUserID, "Banned by admin/owner via Sankarea bot", 0)
 }
