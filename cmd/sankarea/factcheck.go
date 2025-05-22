1990d1685d2d3cf4e888b74036b312f736a7f331
NullMe <192708205+NullMeDev@users.noreply.github.com>
2025-05-22 01:50:25 -0400

    Update factcheck.go

--- cmd/sankarea/factcheck.go
+++ cmd/sankarea/factcheck.go
@@ -1,7 +1,8 @@
 package main
 
-// factCheck is a stub for integrating a fact-check API
+import "fmt"
+
+// factCheck is a placeholder for a real API call
 func factCheck(claim string) string {
-    // TODO: integrate with your preferred API
-    return fmt.Sprintf("Fact-check for '%s': [TODO]", claim)
+    return fmt.Sprintf("Fact-check for '%s': [TODO integrate API]", claim)
 }
