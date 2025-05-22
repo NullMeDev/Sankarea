813f4646274ec0c6218f22f2352f8f6efce6452e
NullMe <192708205+NullMeDev@users.noreply.github.com>
2025-05-21 21:18:37 -0400

    Update articles.go

--- cmd/sankarea/articles.go
+++ cmd/sankarea/articles.go
@@ -1,69 +1,71 @@
-package sankarea
+package main
 
 import (
-    "fmt"
-    "log"
-    "sync"
-    "time"
+	"fmt"
+	"log"
+	"sync"
+	"time"
 
-    "github.com/bwmarrin/discordgo"
-    "github.com/mmcdole/gofeed"
+	"github.com/bwmarrin/discordgo"
+	"github.com/mmcdole/gofeed"
 )
 
 var (
-    articlesLock sync.Mutex
-    articlesSent = make(map[string]bool)
+	articlesLock sync.Mutex
+	articlesSent = make(map[string]bool)
 )
 
-// fetchAndPostArticles fetches articles from active sources and posts to the Discord channel
+// fetchAndPostArticles fetches and posts new articles from active sources to the Discord channel
 func fetchAndPostArticles(dg *discordgo.Session, channelID string, sources []Source, maxPosts int) {
-    articlesLock.Lock()
-    defer articlesLock.Unlock()
+	articlesLock.Lock()
+	defer articlesLock.Unlock()
 
-    if state.Paused {
-        return
-    }
+	if state.Paused {
+		return
+	}
 
-    fp := gofeed.NewParser()
-    postedCount := 0
+	fp := gofeed.NewParser()
+	postedCount := 0
 
-    for _, src := range sources {
-        if !src.Active {
-            continue
-        }
-        feed, err := fp.ParseURL(src.URL)
-        if err != nil {
-            log.Printf("Failed to fetch feed %s: %v", src.Name, err)
-            continue
-        }
+	for _, src := range sources {
+		if !src.Active {
+			continue
+		}
 
-        for _, item := range feed.Items {
-            if postedCount >= maxPosts {
-                break
-            }
-            if articlesSent[item.GUID] {
-                continue
-            }
+		feed, err := fp.ParseURL(src.URL)
+		if err != nil {
+			log.Printf("Failed to fetch feed %s: %v", src.Name, err)
+			continue
+		}
 
-            msg := fmt.Sprintf("**[%s]** *(bias: %s)*\n[%s](%s)", src.Name, src.Bias, item.Title, item.Link)
-            _, err := dg.ChannelMessageSend(channelID, msg)
-            if err != nil {
-                log.Printf("Failed to post article from %s: %v", src.Name, err)
-                continue
-            }
+		for _, item := range feed.Items {
+			if postedCount >= maxPosts {
+				break
+			}
+			if articlesSent[item.GUID] {
+				continue
+			}
 
-            articlesSent[item.GUID] = true
-            postedCount++
-        }
-    }
-    log.Printf("Posted %d articles", postedCount)
-    state.FeedCount = postedCount
-    saveState(state)
+			msg := fmt.Sprintf("**[%s]** *(bias: %s)*\n[%s](%s)", src.Name, src.Bias, item.Title, item.Link)
+			_, err := dg.ChannelMessageSend(channelID, msg)
+			if err != nil {
+				log.Printf("Failed to post article from %s: %v", src.Name, err)
+				continue
+			}
+
+			articlesSent[item.GUID] = true
+			postedCount++
+		}
+	}
+
+	log.Printf("Posted %d articles", postedCount)
+	state.FeedCount = postedCount
+	saveState(state)
 }
 
-// clearArticlesSent resets the tracking of sent articles (useful on bot restart)
+// clearArticlesSent clears the map tracking sent articles (use on restart)
 func clearArticlesSent() {
-    articlesLock.Lock()
-    defer articlesLock.Unlock()
-    articlesSent = make(map[string]bool)
+	articlesLock.Lock()
+	defer articlesLock.Unlock()
+	articlesSent = make(map[string]bool)
 }
